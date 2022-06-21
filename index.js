13d0d73c4d5130f7cb919e8c5b3176bf5909cdf6
Brian Ojeda <80053280+Icey2862@users.noreply.github.com>
2022-06-20 22:48:17 -0300

    Primer-desafio-complementario

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let nombre = prompt("Ingrese el nombre del alumno")
 let apellido = prompt ("Ingrese el apellido del alumno")
-let numeroCalificaciones = parseInt(prompt("Ingrese cantidad de notas a promediar"))
+let calificaciones = parseInt(prompt("Ingrese cantidad de notas a promediar"))
 let suma = 0;
 let promedio = 0;
 
-for (let i = 1; i <= numeroCalificaciones; i++){
+for (let i = 1; i <= calificaciones; i++){
     let calificacion = parseFloat(prompt("Ingresa calificación n°" + i));
     suma = suma + calificacion;
 }
-promedio = suma / numeroCalificaciones;
+promedio = suma / calificaciones;
 
 alert("El promedio del alumno " + nombre + " " + apellido + " es: " + promedio)
